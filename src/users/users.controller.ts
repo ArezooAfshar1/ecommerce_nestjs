4246729551fd3fe73cffde69d795eb1bcfb9947a
arezoo <arezooafshar108@gmail.com>
2026-03-05 17:12:56 +0330

    create finding users route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Res, Param, Delete, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Query, Res, Param, Delete, HttpStatus } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import UserRoleEnum from './enums/userRoleEnum';
+import { Response } from 'express';
 
 @Controller('users')
 export class UsersController {
@@ -19,8 +21,19 @@ export class UsersController {
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  async findAll(
+      @Res() res: Response,
+      @Query('role') role?: UserRoleEnum,
+      @Query('limit') limit: number = 10,
+      @Query('page') page: number = 1,
+  ) {
+    const users = await this.usersService.findAll(role, limit, page)
+
+        return{
+      statusCode: HttpStatus.CREATED,
+      data: users,
+      message: "کاربر با موفقیت دریافت شد"
+    }
   }
 
   @Get(':id')
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import UserRoleEnum from './enums/userRoleEnum';
 
 @Injectable()
 export class UsersService {
@@ -17,8 +18,16 @@ export class UsersService {
     return this.userRepository.save(user);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(role?: UserRoleEnum ,limit: number = 10,  page: number = 1) {
+    const query = this.userRepository.createQueryBuilder('users')
+
+    if (role){
+      query.where('role = :role', {role})
+    }
+
+    query.skip((page - 1) * limit).take(limit)
+
+    return await query.getMany();
   }
 
   findOne(id: number) {
