f5953105b7d48d1cbe2af320cdbc2d7c8480eff0
arezoo <arezooafshar108@gmail.com>
2026-03-08 13:21:17 +0330

    apply logger middleware on 2 routes of products module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { LoggerMiddleware } from 'src/middlewares/logger/logger.middleware';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
 })
-export class ProductsModule {}
+export class ProductsModule {
+  configure( consumer: MiddlewareConsumer){
+    consumer
+    .apply(LoggerMiddleware)
+    .forRoutes(
+      { path: 'products', method: RequestMethod.POST },
+      { path: 'products/:id', method: RequestMethod.GET }
+    )
+  }
+}
